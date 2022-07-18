662ef151c1bc2868dee47f7b1867e376d3811ad0
Mugen <melton.sheridan@gmail.com>
2022-07-18 01:59:49 -0400

    setting up Navigation component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,12 @@ import Intro from "./components/Header/Intro";
 import About from "./components/about/About";
 import ProjectList from "./components/Project_List/Project.list";
 import Contact from "./components/Footer/Contact";
+import Navigation from "./components/Navigation/Nav";
 
 const App = () => {
   return (
   <div>
+    <Navigation/>
     <Intro/>
     <About/>
     <ProjectList/>
---
+++ portfolio/src/components/Navigation/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems"
+
+class Navigation extends Component {
+    render() {
+        return(
+            <nav className='nav-Items'>
+                <h1 className='nav-logo'>React</h1>
+                <div className='menu-icon'>
+
+                </div>
+                <ul>
+                    <li><a href=''></a></li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navigation
