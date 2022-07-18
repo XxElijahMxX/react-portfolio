2f82a5b770c6a38baca00fc0e986e480862d81d7
Mugen <melton.sheridan@gmail.com>
2022-07-18 03:10:45 -0400

    setting up styling for nav bar

--- portfolio/src/components/Navigation/Nav.js
+++ portfolio/src/components/Navigation/Nav.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems"
+import "./nav.css"
 
 class Navigation extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    } 
+
     render() {
         return(
             <nav className='nav-Items'>
-                <h1 className='nav-logo'>React</h1>
-                <div className='menu-icon'>
-
+                <h1 className='nav-logo'>Sheridan's Portfolio</h1>
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>    
                 </div>
                 <ul>
-                    <li><a href=''></a></li>
+                    {MenuItems.map((item, index) => {
+                       return (
+                        <li key={index}>
+                            <a className={item.cName} href={item.url}>
+                                {item.title}   
+                                </a>
+                            </li>
+                       )
+                    })}
                 </ul>
             </nav>
         )
