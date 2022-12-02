47cf7f427b55ee53711bfc7d067a41828623c235
Mugen <melton.sheridan@gmail.com>
2022-12-02 12:33:02 -0500

    added more css to the hero section for custom media screen sizes

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../../src/App.css";
+import Hero from "../hero";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
